ab57e7a4bcc78e12d87b8d6291f6fec62389cd2e
gift-ndoma <giftndoma87@gmail.com>
2025-06-01 07:19:36 +0100

    initial commit

--- scripts/product_page.js
+++ scripts/product_page.js
@@ -12,18 +12,28 @@ function showTab(tabId, clickedButton) {
 const products = [
     {
         name: "Sun-Dried Plantain Chips",
-        price: "$5.99",
+        price: 5.99,
         image: "images/plantain.jpg"
     },
     {
         name: "Fresh Apple Fruit",
-        price: "$5.99",
-        image: "images/plantain.jpg"
+        price: 5.99,
+        image: "images/apple.jpg"
     },
     {
-        name: "Sun-Dried Plantain Chips",
-        price: "$5.99",
-        image: "images/plantain.jpg"
+        name: "Fresh Diary Milk",
+        price: 5.99,
+        image: "images/diary-milk.jpg"
+    },
+    {
+        name: "Fresh Organic Tomato",
+        price: 6.55,
+        image: "images/tomato.jpg"
+    },
+    {
+        name: "Natural Fresh Mango",
+        price: 10.22,
+        image: "images/mango.jpg"
     }
 ]
 
@@ -36,7 +46,7 @@ products.forEach(product => {
     productCard.innerHTML = `
     <img src="${product.image}" alt="${product.name}">
     <h2>${product.name}</h2>
-    <p>${product.price}</p>
+    <p>$${product.price}</p>
     <button id="add_to_cart" class="addToCart" type="button" onclick="addToCart()">Add To Cart</button>
     `;
 
